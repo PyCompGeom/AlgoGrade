fd77534ae49c4839b3f135e8c09d98185250e093
Artyom Fisunenko <artyom.fisunenko@gmail.com>
2024-05-26 18:26:16 +0300

    fix description

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     version="1.1.0",
     author="artandfi (Artem Fisunenko)",
     author_email="artyom.fisunenko@gmail.com",
-    description="A library for automated grading of algorithmic tasks with grading of their intermediate stages. "
-    "The grading of some computational geometry tasks is provided out of the box.",
+    description="A library for automated grading of algorithm-based assignments with grading of their intermediate stages. "
+    "The grading of some computational geometry assignments is provided out of the box.",
     packages=find_packages(),
     install_requires=[line.strip() for line in open("requirements.txt", "r").readlines()],
     keywords=[
